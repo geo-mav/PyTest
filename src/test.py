8c56677980b7b5e432bcb2636c1fdcb1beb593d9
geo mav <geomav1313@gmail.com>
2018-10-15 22:34:02 +0300

    Initial commit

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+data = pd.read_csv(url, header=None)
+data.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation',
+                'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+                'income']
+
+print(data.head(10))
